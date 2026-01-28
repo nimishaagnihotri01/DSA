be5d0bdf6db15f06a569cc9604aeeb04a32d07db
Nimisha Agnihotri <164503566+nimishaagnihotri01@users.noreply.github.com>
2026-01-28 18:45:32 +0000

    index of first string

---
+++ easy/index.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        if (needle.length() == 0) {
+            return 0;
+        }
+        
+        int n = haystack.length();
+        int m = needle.length();
+        
+        if (m > n) {
+            return -1;
+        }
+        
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+            
+            for (j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            
+            if (j == m) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
